d75b533d03ea076607cfcb2b91b6d42d0311448c
betteradcwins <brenohenriqueof@gmail.com>
2019-11-19 11:53:20 -0300

    modified classes Clientes, Associados, Endereco and Agencia

--- project/model/Clientes.java
+++ project/model/Clientes.java
@@ -1,24 +1,45 @@
 package model;
 
+import java.util.Arrays;
 import java.util.Date;
 
-public class Clientes extends Associados 
-{
+public class Clientes extends Associados {
+	
 	private String escolaridade;
-		
-	public Clientes(String nome, String cpf, Date dataNascimento, Character sexo, String rg, String estadoCivil, 
-			String escolaridade){
-		
-		//super(nome, cpf, dataNascimento, sexo, rg, estadoCivil);
-		this.escolaridade = escolaridade;
+	private Conta[] contas = new Conta[3];
+	
+	
+	public Clientes(String nome, String cpf, Date dataNascimento, Character sexo, String rg, String estadoCivil,
+			Agencia agencia, Endereco endereco, String escolaridade, Conta[] contas) {
+		super(nome, cpf, dataNascimento, sexo, rg, estadoCivil, agencia, endereco);
+		setContas(contas);
+		setEscolaridade(escolaridade);
 	}
-	protected String getEscolaridade() 
-	{
+
+	protected Conta[] getContas() {
+		return contas;
+	}
+
+	protected void setContas(Conta[] contas) {
+		this.contas = contas;
+	}
+
+	protected String getEscolaridade() {
 		return escolaridade;
 	}
-	protected void setEscolaridade(String escolaridade) 
-	{
+
+	protected void setEscolaridade(String escolaridade) {
 		this.escolaridade = escolaridade;
 	}
+
+	public String verDados() {
+		return "Clientes [escolaridade=" + escolaridade + ", contas=" + Arrays.toString(contas) + ", getAgencia()="
+				+ getAgencia() + ", getEndereco()=" + getEndereco() + ", getNome()=" + getNome() + ", getCpf()="
+				+ getCpf() + ", getDataNascimento()=" + getDataNascimento() + ", getSexo()=" + getSexo() + ", getRg()="
+				+ getRg() + ", getEstadoCivil()=" + getEstadoCivil() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
+	
 	
 }
--- project/model/Endereco.java
+++ project/model/Endereco.java
@@ -11,47 +11,58 @@ public class Endereco {
 	
 	public Endereco(String cep, Integer numEnd, String rua, String bairro, String cidade, String estado) {
 		super();
-		this.cep = cep;
-		this.numEnd = numEnd;
-		this.rua = rua;
-		this.bairro = bairro;
-		this.cidade = cidade;
-		this.estado = estado;
+		setBairro(bairro);
+		setCep(cep);
+		setCidade(cidade);
+		setEstado(estado);
+		setNumEnd(numEnd);
+		setRua(rua);
 	}
 
 	protected String getCep() {
 		return cep;
 	}
+	
 	protected void setCep(String cep) {
 		this.cep = cep;
 	}
+	
 	protected Integer getNumEnd() {
 		return numEnd;
 	}
+	
 	protected void setNumEnd(Integer numEnd) {
 		this.numEnd = numEnd;
 	}
+	
 	protected String getRua() {
 		return rua;
 	}
+	
 	protected void setRua(String rua) {
 		this.rua = rua;
 	}
+	
 	protected String getBairro() {
 		return bairro;
 	}
+	
 	protected void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+	
 	protected String getCidade() {
 		return cidade;
 	}
+	
 	protected void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+	
 	protected String getEstado() {
 		return estado;
 	}
+	
 	protected void setEstado(String estado) {
 		this.estado = estado;
 	}
