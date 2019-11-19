491a14725c463b62aa0fff0f69214ced6a6a1bd9
betteradcwins <brenohenriqueof@gmail.com>
2019-11-19 11:46:45 -0300

    modified  administrador, gerente and funcionarios classes

--- project/model/Administrador.java
+++ project/model/Administrador.java
@@ -1,1 +1,31 @@
 package model;
+
+public class Administrador {
+	
+	private String usuario = "groot";
+	private String senha = "groot";
+	
+	public Administrador(String usuario, String senha) {
+		super();
+		setSenha(senha);
+		setUsuario(usuario);
+	}
+
+	protected String getUsuario() {
+		return usuario;
+	}
+	
+	protected void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	protected String getSenha() {
+		return senha;
+	}
+	
+	protected void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
