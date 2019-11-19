2d159045bf708e51816d173f11ddf24421e30ba9
betteradcwins <brenohenriqueof@gmail.com>
2019-11-19 11:46:45 -0300

    modified  administrador, gerente and funcionarios classes

--- project/model/Funcionarios.java
+++ project/model/Funcionarios.java
@@ -1,34 +1,46 @@
 package model;
 
-public class Funcionarios extends Associados {
+import java.util.Date;
 
+public class Funcionarios extends Associados {
+	
 	private String ctps;
 	private String cargoEmpresa;
 	private Double salario;
-
-	public Funcionarios(String ctps, String cargoEmpresa, Double salario) {
-		
-                //super(nome, cpf, dataNascimento, sexo, rg, estadoCivil);
-		this.ctps = ctps;
-		this.cargoEmpresa = cargoEmpresa;
-		this.salario = salario;
+	
+	
+	
+	public Funcionarios(String nome, String cpf, Date dataNascimento, Character sexo, String rg, String estadoCivil,
+			Agencia agencia, Endereco endereco, String ctps, String cargoEmpresa, Double salario) {
+		super(nome, cpf, dataNascimento, sexo, rg, estadoCivil, agencia, endereco);
+		setCtps(ctps);
+		setCargoEmpresa(cargoEmpresa);
+		setSalario(salario);
 	}
+
 	protected String getCtps() {
 		return ctps;
 	}
+	
 	protected void setCtps(String ctps) {
 		this.ctps = ctps;
 	}
+	
 	protected String getCargoEmpresa() {
 		return cargoEmpresa;
 	}
+	
 	protected void setCargoEmpresa(String cargoEmpresa) {
 		this.cargoEmpresa = cargoEmpresa;
 	}
+	
 	protected Double getSalario() {
 		return salario;
 	}
+	
 	protected void setSalario(Double salario) {
 		this.salario = salario;
 	}
+	
+	
 }
--- project/model/Gerente.java
+++ project/model/Gerente.java
@@ -2,25 +2,31 @@ package model;
 
 import java.util.Date;
 
-public class Gerente {
+public class Gerente extends Funcionarios {
 
-	 private Date dataIngresso;
-	 private boolean formacao;
-	
-	public Gerente(Date dataIngresso, boolean formacao) {
-		super();
-		this.dataIngresso = dataIngresso;
-		this.formacao = formacao;
+	private Date dataIngresso;
+	private boolean formacao;
+
+	public Gerente(String nome, String cpf, Date dataNascimento, Character sexo, String rg, String estadoCivil,
+			Agencia agencia, Endereco endereco, String ctps, String cargoEmpresa, Double salario, Date dataIngresso,
+			boolean formacao) {
+		super(nome, cpf, dataNascimento, sexo, rg, estadoCivil, agencia, endereco, ctps, cargoEmpresa, salario);
+		setDataIngresso(dataIngresso);
+		setFormacao(formacao);
 	}
+
 	protected Date getDataIngresso() {
 		return dataIngresso;
 	}
+	
 	protected void setDataIngresso(Date dataIngresso) {
 		this.dataIngresso = dataIngresso;
 	}
+	
 	protected boolean isFormacao() {
 		return formacao;
 	}
+	
 	protected void setFormacao(boolean formacao) {
 		this.formacao = formacao;
 	}
