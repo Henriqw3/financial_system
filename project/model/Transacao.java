639c3ef3b6570bd41846b10fddb85eee1c54b5d4
betteradcwins <brenohenriqueof@gmail.com>
2019-11-19 11:58:12 -0300

    modified class Transacao

--- project/model/Transacao.java
+++ project/model/Transacao.java
@@ -1,1 +1,42 @@
 package model;
+
+import java.util.Date;
+
+public class Transacao {
+	
+	private Date data;
+	private Double valor;
+	private String canal;
+	
+	public Transacao(Date data, Double valor, String canal) {
+		super();
+		setCanal(canal);
+		setData(data);
+		setValor(valor);
+	}
+
+	protected Date getData() {
+		return data;
+	}
+	
+	protected void setData(Date data) {
+		this.data = data;
+	}
+	
+	protected Double getValor() {
+		return valor;
+	}
+	
+	protected void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	protected String getCanal() {
+		return canal;
+	}
+	
+	protected void setCanal(String canal) {
+		this.canal = canal;
+	}
+	
+}
